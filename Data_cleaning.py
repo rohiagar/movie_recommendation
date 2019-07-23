f0a1ef83890a42a65fcd643e2b9afc62ebfebe99
rohiagar <52176156+rohiagar@users.noreply.github.com>
2019-07-23 12:57:47 -0400

    Add files via upload
    Here in data cleaning step we create a numpy array and add whole array as column into main dataframe.

---
+++ Data_cleaning.py
@@ -0,0 +1,30 @@
+# Here we import movie by creating a numpy array with correct length and add whole array as column into main dataframe
+df_nan = pd.DataFrame(pd.isnull(df.Rating))
+df_nan = df_nan[df_nan['Rating'] == True]
+df_nan = df_nan.reset_index()
+
+movie_np = []
+movie_id = 1
+
+for i,j in zip(df_nan['index'][1:],df_nan['index'][:-1]):
+    # numpy approach
+    temp = np.full((1,i-j-1), movie_id)
+    movie_np = np.append(movie_np, temp)
+    movie_id += 1
+
+# Account for last record and corresponding length
+# numpy approach
+last_record = np.full((1,len(df) - df_nan.iloc[-1, 0] - 1),movie_id)
+movie_np = np.append(movie_np, last_record)
+
+print('Movie numpy: {}'.format(movie_np))
+print('Length: {}'.format(len(movie_np)))
+
+
+# remove those Movie ID rows
+df = df[pd.notnull(df['Rating'])]
+
+df['Movie_Id'] = movie_np.astype(int)
+df['Cust_Id'] = df['Cust_Id'].astype(int)
+print('-Dataset examples-')
+print(df.iloc[::5000000, :])
