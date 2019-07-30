ea74fc63c7ca0892818bf39fb12b3c2e53b86c46
Akhila127 <52205815+Akhila127@users.noreply.github.com>
2019-07-30 13:04:41 -0400

    Add files via upload
    Here, we do the prediction with Pearson's R correlation. 
    It does the prediction with a single movie, Suppose you say you like titanic the  with help of Pearson's R correlation you get other 10 recommendations that you might like.

---
+++ Prediction with Pearson's R correlation.py
@@ -0,0 +1,17 @@
+#lets recommend movies with Pearson's R model
+
+def recommend(movie_title, min_count):
+    print("For movie ({})".format(movie_title))
+    print("- Top 10 movies recommended based on Pearsons'R correlation - ")
+    i = int(df_title.index[df_title['Name'] == movie_title][0])
+    target = df_p[i]
+    similar_to_target = df_p.corrwith(target)
+    corr_target = pd.DataFrame(similar_to_target, columns = ['PearsonR'])
+    corr_target.dropna(inplace = True)
+    corr_target = corr_target.sort_values('PearsonR', ascending = False)
+    corr_target.index = corr_target.index.map(int)
+    corr_target = corr_target.join(df_title).join(df_movie_summary)[['PearsonR', 'Name', 'count', 'mean']]
+    print(corr_target[corr_target['count']>min_count][:10].to_string(index=False))
+
+
+
